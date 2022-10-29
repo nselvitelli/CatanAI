52c77b48396b2a561fce94cf2c14af9d4b712d53
tom-weatherbee <113213237+tom-weatherbee@users.noreply.github.com>
2022-10-29 17:32:13 -0400

    abstract class action

--- Actions/Action.py
+++ Actions/Action.py
@@ -1,4 +1,11 @@
-class Action:
+from abc import ABC, abstractmethod
+
+
+class Action(ABC):
 
     def __init__(self) -> None:
+        return
+
+    @abstractmethod
+    def apply(self, state):
         pass
