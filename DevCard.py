b2cf845251ceaa2990fba4d8fdff16911db1aced
samphillippoND <sphillippo@nextdroid.com>
2022-10-29 14:12:08 -0400

    finishes PlayerData, adds DevCard

---
+++ DevCard.py
@@ -0,0 +1,4 @@
+class DevCard:
+
+    def __init__(self, action) -> None:
+        self.action = action
--- PlayerData.py
+++ PlayerData.py
@@ -5,15 +5,17 @@ from Resource import Resource
 
 class PlayerData:
 
-    def __init__(self) -> None:
+    def __init__(self, wheat=0, brick=0, log=0, ore=0, sheep=0, devCards=[]) -> None:
         self.resourcesAvailable = {}
-        self.resourcesAvailable[Resource.WHEAT] = 0
-        self.resourcesAvailable[Resource.BRICK] = 0
-        self.resourcesAvailable[Resource.LOG] = 0
-        self.resourcesAvailable[Resource.ORE] = 0
-        self.resourcesAvailable[Resource.SHEEP] = 0
+        self.resourcesAvailable[Resource.WHEAT] = wheat
+        self.resourcesAvailable[Resource.BRICK] = brick
+        self.resourcesAvailable[Resource.LOG] = log
+        self.resourcesAvailable[Resource.ORE] = ore
+        self.resourcesAvailable[Resource.SHEEP] = sheep
 
-
+        self.devCards = []
+        for devCard in devCards:
+            self.devCards.push(devCard)
 
 class PlayerColor(Enum):
     WHITE = 0
