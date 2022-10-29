eceb0ecd35b159b1d05a4670102e5740e5298ce6
tom-weatherbee <113213237+tom-weatherbee@users.noreply.github.com>
2022-10-29 18:15:47 -0400

    added get port actions

--- Actions/Trade.py
+++ Actions/Trade.py
@@ -1,8 +1,9 @@
 from Actions.Action import Action
 
 
-class BuildCity(Action):
+class Trade(Action):
 
-    def __init__(self, resource) -> None:
+    def __init__(self, resource, quantity) -> None:
         super().__init__()
         self.resource = resource
+        self.quantity = quantity
--- State.py
+++ State.py
@@ -2,6 +2,8 @@ from Actions.BuildCity import BuildCity
 from Actions.BuildRoad import BuildRoad
 from Actions.BuildSettlement import BuildSettlement
 from Actions.DevelopmentCard import DevelopmentCard
+from Actions.Trade import Trade
+from Node import Port
 
 from Resource import Resource
 
@@ -29,6 +31,8 @@ class State:
         if resourcesAvailable(0,0,1,1,1):
             validActions.append(DevelopmentCard(currentPlayer))
 
+        validActions.extend(self.getPortActions(currentPlayer))
+
     def getBuildingAction(self, brick, log, sheep, wheat, ore, currentPlayer, resourcesAvailable, func, action):
         if resourcesAvailable(brick, log, sheep, wheat, ore, resourcesAvailable):
             valid = set()
@@ -41,8 +45,22 @@ class State:
     def resourcesAvailable(self, brick, log, sheep, wheat, ore, resourcesAvailable):
         return resourcesAvailable[Resource.BRICK] >= brick and resourcesAvailable[Resource.LOG] >= log and resourcesAvailable[Resource.SHEEP] >= sheep and resourcesAvailable[Resource.WHEAT] >= wheat and resourcesAvailable[Resource.ORE] >= ore
 
-    def portsWithSettlement(self):
+    def getPortActions(self, currentPlayer):
+        actions = []
 
-
-
-# end State
+        ports = set()
+        for settlementNodeID in currentPlayer.firstSettlements:
+            ports.union(set(self.board.bfsPorts(settlementNodeID)))
+        maxTradeQuantity = 3 if Port.THREE_TO_ONE else 4
+        inaccessiblePorts = set([Port.BRICK, Port.LOG, Port.ORE, Port.SHEEP, Port.WHEAT]) - ports
+        portsToResources = {Port.BRICK : Resource.BRICK, Port.LOG : Resource.LOG, Port.ORE : Resource.ORE, Port.SHEEP : Resource.SHEEP, Port.WHEAT : Resource.SHEEP}
+        for port in ports - set([Port.THREE_TO_ONE]):
+            resource = portsToResources[port]
+            if currentPlayer.resourcesAvailable[resource] >= 2:
+                actions.append(Trade(resource, 2))
+        for port in inaccessiblePorts - set([Port.THREE_TO_ONE]):
+            resource = portsToResources[port]
+            if currentPlayer.resourcesAvailable[resource] >= maxTradeQuantity:
+                actions.append(Trade(resource, maxTradeQuantity))
+        
+        return actions
