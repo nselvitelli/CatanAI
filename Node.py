ff072f14ab65d035b9d82db05fb2093b06f658f5
tom-weatherbee <113213237+tom-weatherbee@users.noreply.github.com>
2022-10-29 14:48:41 -0400

    added ids to nodes

--- Node.py
+++ Node.py
@@ -4,13 +4,20 @@ from PlayerData import PlayerColor
 
 class Node:
 
-    def __init__(self, edges, tiles, piece) -> None:
+    def __init__(self, edges, tiles, piece, id) -> None:
         self.piece = piece # tuple of type and color
         self.edges = edges
         self.tiles = tiles
+        self.id = id
 
-    def __init__(self, edges, tiles) -> None:
-        self.__init__(edges, tiles, (NodePiece.EMPTY, PlayerColor.BLANK))
+    def __init__(self, edges, tiles, id) -> None:
+        self.__init__(edges, tiles, (NodePiece.EMPTY, PlayerColor.BLANK), id)
+
+    def getTreeEndpoints(self, playerColor):
+        self.getTreeEndpointsWithExplored(playerColor, [])
+
+    def getTreeEndpointsWithExplored(self, playerColor, explored):
+        pass
 
 
 class Edge:
