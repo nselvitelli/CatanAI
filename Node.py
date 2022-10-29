e2e1630ac4a9e37394a754dca14d84138cf56564
tom-weatherbee <113213237+tom-weatherbee@users.noreply.github.com>
2022-10-29 16:42:20 -0400

    added bfs to node for settlement and city

--- Node.py
+++ Node.py
@@ -27,19 +27,65 @@ class Node:
             endpoints.extend(edgeMap[edge].bfsEndpoints(playerColor, explored + [self.id], self.id, edgeMap, nodeMap))
         return endpoints
 
+    def bfsPossibleSettlements(self, playerColor, explored, edgeMap, nodeMap):
+        if self.id in explored:
+            return []
+
+        if self.piece == (NodePiece.EMPTY, PlayerColor.BLANK):
+            for edge in self.edges:
+                if edge.adjacentSettlement(self.id, edgeMap, nodeMap):
+                    return []
+            return [self.id]
+        
+        possibles = []
+        for edge in self.edges:
+            possibles.extend(edgeMap[edge].bfsPossibleSettlements(self, playerColor, explored, edgeMap, nodeMap))
+        return possibles
+
+    def bfsUpgradeableSettlements(self, playerColor, explored, edgeMap, nodeMap):
+        if self.id in explored:
+            return []
+
+        if self.piece == (NodePiece.SETTLEMENT, playerColor):
+            return [self.id]
+        
+        possibles = []
+        for edge in self.edges:
+            possibles.extend(edgeMap[edge].bfsUpgradeableSettlements(self, playerColor, explored, edgeMap, nodeMap))
+        return possibles
+
 
 class Edge:
 
-    def __init__(self, id, playerColor=PlayerColor.BLANK) -> None:
+    def __init__(self, id, nodeOne, nodeTwo, playerColor=PlayerColor.BLANK) -> None:
         self.playerColor = playerColor
         self.id = id
+        self.nodeOne = nodeOne
+        self.nodeTwo = nodeTwo
 
-    # 
     def bfsEndpoints(self, playerColor, explored, comingFrom, nodeMap, edgeMap):
         if self.playerColor == PlayerColor.BLANK:
             return [self.id]
         elif self.playerColor == playerColor:
-            otherNode = self.id-comingFrom
+            otherNode = self.nodeOne if self.nodeTwo == comingFrom else self.nodeTwo
             return nodeMap[otherNode].bfsEndpoints(playerColor, explored, nodeMap, edgeMap)
+        else:
+            return []
+
+    def bfsPossibleSettlements(self, playerColor, explored, comingFrom, nodeMap, edgeMap):
+        if self.playerColor == playerColor:
+            otherNode = self.nodeOne if self.nodeTwo == comingFrom else self.nodeTwo
+            return nodeMap[otherNode].bfsPossibleSettlements(playerColor, explored, nodeMap, edgeMap)
+        else:
+            return []
+
+    def adjacentSettlement(self, comingFrom, nodeMap, edgeMap):
+        otherNode = self.nodeOne if self.nodeTwo == comingFrom else self.nodeTwo
+        return nodeMap[otherNode].piece[0] != NodePiece.EMPTY
+
+    def bfsUpgradeableSettlements(self, playerColor, explored, comingFrom, nodeMap, edgeMap):
+        if self.playerColor == playerColor:
+            otherNode = self.nodeOne if self.nodeTwo == comingFrom else self.nodeTwo
+            return nodeMap[otherNode].bfsUpgradeableSettlements(playerColor, explored, nodeMap, edgeMap)
         else:
             return []
