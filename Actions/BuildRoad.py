4dc81a30e5519bad203d2a15914b3b4cef47dd72
samphillippoND <sphillippo@nextdroid.com>
2022-10-29 18:10:01 -0400

    starts build road action

--- Actions/BuildRoad.py
+++ Actions/BuildRoad.py
@@ -1,8 +1,24 @@
+from hashlib import new
 from Actions.Action import Action
+from Board import Board
+from Node import Edge
 
 
+# NOT DONE
 class BuildRoad(Action):
 
-    def __init__(self, edgeID, playerColor) -> None:
+    def __init__(self, edgeID) -> None:
         super().__init__()
         self.edgeID = edgeID
+
+    def apply(self, state):
+        newEdges = {}
+        for edgeID in state.board.edges:
+            if edgeID == self.edgeID:
+                newEdges[edgeID] = Edge(
+                    edgeID, state.board.edges[edgeID].nodeOne, state.board.edges[edgeID].nodeTwo, state.whoseTurn)
+            else:
+                newEdges[edgeID] = Edge(
+                    edgeID, state.board.edges[edgeID].nodeOne, state.board.edges[edgeID].nodeTwo, state.board.edges[edgeID].playerColor)
+        board = Board(state.board.tiles, state.board.nodes,
+                      state.board.edges, self.tileID)
