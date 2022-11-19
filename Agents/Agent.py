43352d1e2e62df973293a0a292073453797704fe
samphillippoND <sphillippo@nextdroid.com>
2022-11-19 16:25:04 -0500

    adds agent inteface

---
+++ Agents/Agent.py
@@ -0,0 +1,9 @@
+class Agent:
+
+    def __init__(self, playerColor):
+        self.color = playerColor
+
+    def getAction(self, state):
+        """
+        The Agent will receive a GameState and must return an action
+        """
