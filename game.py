324c53af24e5db19fcc4bc23d9efa19b9f6e4f1a
samphillippoND <sphillippo@nextdroid.com>
2022-11-19 16:01:11 -0500

    good size window

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import time
 
 class Game:
     """
@@ -12,10 +13,13 @@ class Game:
         """
         Main control loop for game play.
         """
-        self.display.initialize(self.state)
-
+        # self.display.initialize(self.state)
+        # time.sleep(0.5)
         while True:
-            x = 1
+            self.display.initialize(self.state)
+            time.sleep(2)
+            self.display.finish()
+            time.sleep(2)
 
         # while not self.gameOver:
         # here is where we actually play game
