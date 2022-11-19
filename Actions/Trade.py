6e3b9e74d3fb447abbae9519bf3536c7f8604540
nickselvitelli <nickselvitelli>
2022-11-19 14:28:34 -0500

    trade action done

--- Actions/Trade.py
+++ Actions/Trade.py
@@ -1,9 +1,22 @@
 from Actions.Action import Action
+from Node import Port
 
 
 class Trade(Action):
 
-    def __init__(self, resource, quantity) -> None:
+    def __init__(self, resource, quantity, targetResource) -> None:
         super().__init__()
         self.resource = resource
         self.quantity = quantity
+        self.targetResource = targetResource
+
+
+    def apply(self, state):
+        newState = state.getCopy()
+
+        playerData = newState.playerDataDict[newState.whoseTurn]
+        playerData[self.resource] -= self.quantity
+        playerData[self.targetResource] += 1
+
+
+        return newState
