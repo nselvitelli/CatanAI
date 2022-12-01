01dd225270d906fc9b4ec88c46049cd984a02937
samphillippoND <sphillippo@nextdroid.com>
2022-11-30 20:23:54 -0500

    alpha beta fixes

--- Agents/AlphaBetaAgent.py
+++ Agents/AlphaBetaAgent.py
@@ -1,6 +1,7 @@
 from Agents.Agent import Agent
 from Agents.EvalFunctions import evalFuncVP
 import math
+import random
 
 
 class AlphaBetaAgent(Agent):
@@ -12,55 +13,65 @@ class AlphaBetaAgent(Agent):
 
     def getAction(self, state):
         """
-        Returns the minimax action using self.depth and self.evaluationFunction
+        Do Minimax to self.depth with self.evalFunc
         """
 
         actions = state.getValidActions()
-        bestAction = actions[0]
+
+        if len(actions) == 1:
+            return actions[0]
+
+        bestActions = []
+
         value = -1 * math.inf
         alpha = -1 * math.inf
         beta = math.inf
         for action in actions:
             nextState = action.apply(state)
             minimaxVal = self.minimax(
-                nextState, self.depth, state.whoseTurn, alpha, beta)
+                nextState, self.depth, state.whoseTurn, -1, alpha, beta)
             if minimaxVal > value:
-                bestAction = action
                 value = minimaxVal
+                bestActions = [action]
+            elif minimaxVal == value:
+                bestActions.append(action)
             alpha = max(alpha, minimaxVal)
+
+        bestAction = bestActions[random.randint(0, len(bestActions) - 1)]
 
         print("Minimax Agent ", self.color.name,
               "chose action ", bestAction.getActionAsString())
 
         return bestAction
 
-    def minimax(self, gameState, depth, maxPlayerIndex, alpha, beta):
+    def minimax(self, gameState, depth, maxPlayerIndex, prevPlayerActionIndex, alpha, beta):
         agentIndex = gameState.whoseTurn
+
         actions = gameState.getValidActions()
 
         if gameState.isGameOver() or len(actions) == 0:
             return self.evaluationFunction(gameState)
 
         if agentIndex == maxPlayerIndex:  # Maximize for max player
+            if agentIndex != prevPlayerActionIndex:
+                depth = depth - 1
             if depth == 0:
                 return self.evaluationFunction(gameState)
             value = -1 * math.inf
             for action in actions:
                 nextState = action.apply(gameState)
-                value = max(value, self.minimax(
-                    nextState, depth, maxPlayerIndex, alpha, beta))
+                value = max(value, self.minimax(nextState, depth,
+                            maxPlayerIndex, agentIndex, alpha, beta))
                 if value > beta:
                     return value
                 alpha = max(alpha, value)
             return value
-        else:  # Minimize for ghosts
+        else:  # Minimize for enemy agents
             value = math.inf
             for action in actions:
                 nextState = action.apply(gameState)
-                if nextState.whoseTurn == maxPlayerIndex:
-                    depth = depth - 1
-                value = min(value, self.minimax(
-                    nextState, depth, maxPlayerIndex, alpha, beta))
+                value = min(value, self.minimax(nextState, depth,
+                            maxPlayerIndex, agentIndex, alpha, beta))
                 if value < alpha:
                     return value
                 beta = min(beta, value)
