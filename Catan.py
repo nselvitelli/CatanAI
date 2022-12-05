8e1f9defdd7cd36b21aebc9b27457b7b17f3a672
nickselvitelli <nickselvitelli>
2022-12-05 15:58:28 -0500

    time check

--- Catan.py
+++ Catan.py
@@ -45,10 +45,13 @@ class RunMultipleGames:
         print(bcolors.FAIL + "ERROR:", error, bcolors.ENDC)
 
     def gameProcessCallback(self, result):
+        state = result[0]
+        totalTime = result[1]
+
         self.gamesComplete += 1
         print(bcolors.OKGREEN, "### Completed game", self.gamesComplete, "out of", self.numberGames, "###", bcolors.ENDC)
 
-        players = result.playerDataList
+        players = state.playerDataList
 
         for player in players:
             playerCol = player.color
@@ -56,7 +59,7 @@ class RunMultipleGames:
 
             numSettlements = 0
             numCities = 0
-            playerPieces = list(map(lambda a: result.board.nodes[a].piece[0], player.settlements))
+            playerPieces = list(map(lambda a: state.board.nodes[a].piece[0], player.settlements))
             for piece in playerPieces:
                 if piece == NodePiece.CITY:
                     numCities += 1
@@ -67,14 +70,16 @@ class RunMultipleGames:
                 self.statsMap[playerCol]['VP'] = increaseAverage('VP', vp)
                 self.statsMap[playerCol]['SETTLEMENTS'] = increaseAverage('SETTLEMENTS', numSettlements)
                 self.statsMap[playerCol]['CITIES'] = increaseAverage('CITIES', numCities)
+                self.statsMap[playerCol]['TIME'] = increaseAverage('TIME', totalTime)
             else:
                 self.statsMap[playerCol] = {}
                 self.statsMap[playerCol]['VP'] = vp
                 self.statsMap[playerCol]['SETTLEMENTS'] = numSettlements
                 self.statsMap[playerCol]['CITIES'] = numCities
                 self.statsMap[playerCol]['WINS'] = 0
+                self.statsMap[playerCol]['TIME'] = totalTime
 
-        winnerCol = result.getWinner().color
+        winnerCol = state.getWinner().color
         self.statsMap[winnerCol]['WINS'] += 1
 
     def launchGames(self):
@@ -87,8 +92,8 @@ class RunMultipleGames:
 
         startTime = time.time()
 
+        print(bcolors.OKGREEN, "### Launching",  bcolors.WARNING + str(self.numberGames) + bcolors.OKGREEN  ,"Games with agents:", self.agents, " ###", bcolors.ENDC)
         for i in range(self.numberGames):
-            print(bcolors.OKGREEN, "### Launching Game" + bcolors.WARNING, str(i + 1), bcolors.OKGREEN + "###", bcolors.ENDC)
             random.shuffle(self.agents)
             pool.apply_async(playGame, args=(State.generateState(self.agents),), callback=self.gameProcessCallback, error_callback=self.errorCallback)   
          
@@ -101,10 +106,12 @@ class RunMultipleGames:
             vp = "{:.2f}".format(stats['VP'])
             settlements = "{:.2f}".format(stats['SETTLEMENTS'])
             cities = "{:.2f}".format(stats['CITIES'])
+            gameTime = "{:.2f}".format(stats['TIME'])
             print(bcolors.OKGREEN, color.name, bcolors.OKCYAN + "agent won", wins, "game(s). Win Rate:" + bcolors.WARNING, percent + '%', bcolors.ENDC)
             print(bcolors.OKCYAN, "- average VP:", vp, bcolors.ENDC)
             print(bcolors.OKCYAN, "- average Settlements:", settlements, bcolors.ENDC)
             print(bcolors.OKCYAN, "- average Cities:", cities, bcolors.ENDC)
+            print(bcolors.OKCYAN, "- average game time:", gameTime, "seconds", bcolors.ENDC)
 
         endTime = time.time()
         print(bcolors.OKCYAN, self.gamesComplete, "games completed in" + bcolors.OKGREEN, '{:0.2f}'.format(endTime - startTime), bcolors.OKCYAN + "seconds", bcolors.ENDC)
@@ -116,14 +123,14 @@ if __name__ == '__main__':
 
     agents = [
         # KeyboardAgent(PlayerColor.RED, cheats=True),
-        MiniMaxAgent(PlayerColor.RED, depth=1, evaluationFunction=Agents.EvalFunctions.evalFuncCombineAll, loud=False),
-        MiniMaxAgent(PlayerColor.WHITE, depth=2, evaluationFunction=Agents.EvalFunctions.evalFuncCombineAll, loud=False),
-        MaxiMaxAgent(PlayerColor.BLUE, depth=2, evaluationFunction=Agents.EvalFunctions.evalFuncCombineAll, loud=False),
+        # MiniMaxAgent(PlayerColor.RED, depth=1, evaluationFunction=Agents.EvalFunctions.evalFuncCombineAll, loud=False),
+        # MiniMaxAgent(PlayerColor.WHITE, depth=2, evaluationFunction=Agents.EvalFunctions.evalFuncCombineAll, loud=False),
+        # MaxiMaxAgent(PlayerColor.BLUE, depth=2, evaluationFunction=Agents.EvalFunctions.evalFuncCombineAll, loud=False),
         # MiniMaxAgent(PlayerColor.RED, depth=2, evaluationFunction=Agents.EvalFunctions.createCustomEvalFuncCombineAll([1,1,10,10,20,10,10,10]))
         # AlphaBetaAgent(PlayerColor.BLUE, depth=2, evaluationFunction=Agents.EvalFunctions.evalFuncCombineAll, loud = False)
-        #RandomAgent(PlayerColor.BLUE),
-        # RandomAgent(PlayerColor.ORANGE, loud=False),
-        # RandomAgent(PlayerColor.WHITE, loud=False),
+        RandomAgent(PlayerColor.BLUE, loud=False),
+        RandomAgent(PlayerColor.ORANGE, loud=False),
+        RandomAgent(PlayerColor.WHITE, loud=False),
     ]
 
     #state = State.generateState(agents)
